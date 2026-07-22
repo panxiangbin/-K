d55d814f144ed0256afcaafb11de7153c55e216a
潘斌 <smxpxb008@gmail.com>
2026-07-22 20:06:58 +0800

    加载第三轮UI视觉优化样式

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './ui-responsive.css';
+import './ui-polish.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
