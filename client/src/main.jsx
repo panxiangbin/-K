88636ffca606047a998db65c4c687f01ef10627f
潘斌 <smxpxb008@gmail.com>
2026-07-26 20:35:25 +0800

    feat(ui): install rules help launcher

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import RulesHelpLauncher from './components/RulesHelpLauncher';
 import { installGameTableHeaderEnhancer } from './game-table-header';
 import './index.css';
 import './ui-responsive.css';
@@ -8,6 +9,7 @@ import './ui-polish.css';
 import './waiting-room.css';
 import './game-table-responsive.css';
 import './game-table-header.css';
+import './rules-help.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<><App /><RulesHelpLauncher /></>);
 installGameTableHeaderEnhancer();
