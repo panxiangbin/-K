584a7f984b556a0f0b3b6e620e54d7a478cfcb33
潘斌 <smxpxb008@gmail.com>
2026-07-26 21:26:50 +0800

    feat(ui): install hand interaction enhancer

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import RulesHelpLauncher from './components/RulesHelpLauncher';
 import { installGameTableHeaderEnhancer } from './game-table-header';
+import { installHandInteractionEnhancer } from './hand-interaction-enhancer';
 import './index.css';
 import './ui-responsive.css';
 import './ui-polish.css';
 import './waiting-room.css';
 import './game-table-responsive.css';
 import './game-table-header.css';
+import './hand-interaction.css';
 import './rules-help.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<><App /><RulesHelpLauncher /></>);
 installGameTableHeaderEnhancer();
+installHandInteractionEnhancer();
