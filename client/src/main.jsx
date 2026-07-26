82118a614d2c479061f0d308ca1031b0e6ac8ca0
潘斌 <smxpxb008@gmail.com>
2026-07-26 16:32:17 +0800

    feat(ui): install table header enhancer

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { installGameTableHeaderEnhancer } from './game-table-header';
 import './index.css';
 import './ui-responsive.css';
 import './ui-polish.css';
@@ -8,3 +9,4 @@ import './waiting-room.css';
 import './game-table-responsive.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+installGameTableHeaderEnhancer();
